e90101f9603798ff47f450fa07c6160e6a2c6a19
TurtlePower <agnkvdm@gmail.com>
2023-03-29 11:38:12 +0300

    add saving current time and set it on page reload

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player';
 
+const LOCALSTORAGE_PLAYER_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('[id="vimeo-player"]');
+const videoPlayer = new Vimeo(iframe);
+const savedPlayerTime = JSON.parse(
+  localStorage.getItem(LOCALSTORAGE_PLAYER_KEY)
+);
+
+videoPlayer.setCurrentTime(savedPlayerTime || 0);
+
+videoPlayer.on('timeupdate', onPlay);
+
+function onPlay(currentTime) {
+  localStorage.setItem(
+    LOCALSTORAGE_PLAYER_KEY,
+    JSON.stringify(currentTime.seconds)
+  );
+}
